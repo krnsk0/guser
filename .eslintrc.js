234e1a0db08559ac8446ed38d83f215a41ce5295
krnsk0 <jkurinsky@gmail.com>
2020-08-17 23:08:58 -0400

    install eslint-jest plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ['node', 'prettier'],
   plugins: ['jest'],
+  env: {
+    'jest/globals': true,
+  },
   rules: {
     'no-process-exit': 0,
     'import/no-commonjs': 0,
