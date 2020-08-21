794b9265a25ba520c78c91f4b528cbdc1685a96d
krnsk0 <jkurinsky@gmail.com>
2020-08-21 01:12:33 -0400

    write list users slice

--- src/listUsers/listUsers.js
+++ src/listUsers/listUsers.js
@@ -1,3 +1,22 @@
-const listUsers = () => console.log('list users');
+const file = require('../utils/jsonFile');
 
-module.exports = { listUsers };
+const getLongestUsernameLength = (users) =>
+  users.reduce(
+    (longest, { username }) =>
+      username.length > longest ? username.length : longest,
+    0
+  );
+
+const listUsers = () => {
+  const users = Object.values(file.get());
+  const longestLength = getLongestUsernameLength(users);
+
+  users.forEach(({ username, email }) => {
+    const padding = ' '.repeat(longestLength - username.length + 2);
+    console.log(username + padding + email);
+  });
+
+  return Promise.resolve();
+};
+
+module.exports = { listUsers, getLongestUsernameLength };
