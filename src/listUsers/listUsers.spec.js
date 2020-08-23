8d69f16c17b034079f544d22f4de304cd4b231d1
krnsk0 <jkurinsky@gmail.com>
2020-08-22 23:28:51 -0400

    mock loadUserDetails import

--- src/listUsers/listUsers.spec.js
+++ src/listUsers/listUsers.spec.js
@@ -1,30 +1,30 @@
 const { listUsers } = require('./listUsers');
-const { file } = require('../utils');
+
+jest.mock('../utils', () => {
+  return {
+    loadUserData: () => [
+      { username: 'abcdefg', email: 'abc@def.com', paddedUsername: 'abcdefg' },
+      {
+        username: 'aaa',
+        email: 'aaa@aaa.com',
+        paddedUsername: 'aaa    ',
+      },
+    ],
+  };
+});
 
 describe.only('The listUsers function', () => {
-  const fileGet = file.get();
   const consoleLog = console.log;
   beforeEach(() => {
-    file.get = jest.fn().mockImplementation(() => ({
-      '9YEyhiVx/8iox+zODlakpPCwehg=': {
-        username: 'abcdefg',
-        email: 'abc@def.com',
-      },
-      '0y3f/wnta5PBjxSi3UqaSTfM0FU=': {
-        username: 'aasdf',
-        email: 'asdf@asdf.com',
-      },
-    }));
     console.log = jest.fn();
   });
   afterEach(() => {
-    file.get = fileGet;
     console.log = consoleLog;
   });
 
   it('logs out data that matches a snapshot', () => {
     listUsers();
     expect(console.log.mock.calls[0]).toEqual(['abcdefg  abc@def.com']);
-    expect(console.log.mock.calls[1]).toEqual(['aasdf    asdf@asdf.com']);
+    expect(console.log.mock.calls[1]).toEqual(['aaa      aaa@aaa.com']);
   });
 });
