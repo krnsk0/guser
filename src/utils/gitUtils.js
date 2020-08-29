5b34259aaca172b66144ec08d7edf87d96436feb
krnsk0 <jkurinsky@gmail.com>
2020-08-29 12:21:54 -0400

    write show user func

--- src/utils/gitUtils.js
+++ src/utils/gitUtils.js
@@ -7,4 +7,38 @@ const bailIfGitNotFound = () => {
   }
 };
 
-module.exports = { bailIfGitNotFound };
+const setLocalGitUser = (user, email) => {
+  const command = `git config user.name "${user}" && git config user.email "${email}"`;
+  if (shell.exec(command).code !== 0) {
+    console.log('guser could not set local config');
+  } else {
+    console.log(`guser set local config to ${user}, ${email}`);
+  }
+};
+
+const unsetLocalGitUser = () => {
+  const command = `git config --unset user.name && git config --unset user.email`;
+  if (shell.exec(command).code !== 0) {
+    console.log('guser could not unset local config');
+  } else {
+    console.log(`guser unset local config`);
+  }
+};
+
+const showLocalGitUser = () => {
+  const user = shell.exec(`git config --local user.name`, { silent: true })
+    .stdout;
+  if (user.startsWith('error')) console.log(`no local user set`);
+  else console.log(`local user: ${user}`);
+  const email = shell.exec(`git config --local user.email`, { silent: true })
+    .stdout;
+  if (email.startsWith('error')) console.log(`no local email set`);
+  else console.log(`local email: ${email}`);
+};
+
+module.exports = {
+  bailIfGitNotFound,
+  setLocalGitUser,
+  unsetLocalGitUser,
+  showLocalGitUser,
+};
